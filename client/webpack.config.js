cfd42f7bfdc5e4a9b56e39e71e73fae8c7982de0
myra-k <myrakhatoon90@gmail.com>
2023-09-04 17:10:38 +0100

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,36 +4,35 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// Configuration for Webpack
+
 module.exports = () => {
   return {
-    mode: 'development',// Set the mode to development
+    mode: 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
-    },// Define entry points of application
+    },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
-    }, // Define the output directory and filenames for bundled files
+    }, 
     plugins: [
-      // HTML template generation
+     
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-      // Extract CSS into separate files
+      
       new MiniCssExtractPlugin(),
 
-      // Inject the service worker into the application
+     
       new InjectManifest({
-        swSrc: './src-sw.js', // Path to the service worker source
-        swDest: 'src-sw.js', // Destination filename for the injected service worker
+        swSrc: './src-sw.js', 
+        swDest: 'src-sw.js', 
       }),
 
-      // Generate a Web App Manifest
       new WebpackPwaManifest({
-        // Web App Manifest configuration
+        
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
@@ -55,20 +54,18 @@ module.exports = () => {
 
     module: {
       rules: [
-        // CSS loader configuration
+        
         {
           test: /\.css$/i,
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-        // Asset loader for images
         {
           test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
           type: 'asset/resource',
           generator: {
             filename: 'assets/icons/[name].[ext]'  
-          }// Define the output path and filename for images
+          }
         },
-        // Babel configuration for JavaScript files
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
