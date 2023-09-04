a490e7e71cc2913af2ba6172f2dba6ec69bd8efc
myra-k <myrakhatoon90@gmail.com>
2023-09-04 14:56:29 +0100

    package.json and src added

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,6 +5,8 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+const { StaleWhileRevalidate } = require('workbox-strategies');
+
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -19,12 +21,27 @@ const pageCache = new CacheFirst({
   ],
 });
 
+// Warm the page cache for specific URLs
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// Register route for navigation requests
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Register route for style, script, and worker requests
+registerRoute(
+  //the callback function is defined and filters requests to cache
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // this is the cache storage name
+    cacheName: 'asset-cache',
+    plugins: [
+      // The plugin caches responses with these headers to a max-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
